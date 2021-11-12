b4c4323c45ed9f6cda90b5926c923db1a568897f
Arturo Rosales <arturoi_rosales@hotmail.com>
2021-11-12 16:50:01 -0600

    Show de equipos

--- resources/views/dashboard/teams/show.blade.php
+++ resources/views/dashboard/teams/show.blade.php
@@ -11,7 +11,7 @@
 <div class="container-fluid mt-4">
     <div class="row">
         
-        <div class="col-8">
+        <div class="col-12">
             <div class="card shadow">
                 <div class="card-header border-0">
                     <div class="row align-items-center">
