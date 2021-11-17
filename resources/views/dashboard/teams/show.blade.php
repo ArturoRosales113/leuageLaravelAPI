1c4930ecdf10b5ea76e9c28f1c8fa4d6c2d67ac7
Abel Landa <abel.landa@hotmail.com>
2021-11-16 18:31:17 -0600

    Ajuste diseño modal
    Ajuste diseño modal

--- resources/views/dashboard/teams/show.blade.php
+++ resources/views/dashboard/teams/show.blade.php
@@ -114,13 +114,12 @@
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Crear Jugador</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
-            <div class="col-12">
+            <div class="col-12 border-primary">
                 <div class="card shadow pb-5 mt-4">
                     <div class="card-header border-0">
                         <div class="row align-items-center">
