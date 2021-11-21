bd04bf68f553fc39adb2013086af734d9dc5fb91
Arturo <arturoi_rosales@hotmail.com>
2021-11-20 22:43:13 -0600

    Controllers

--- app/Http/Controllers/ScoringController.php
+++ app/Http/Controllers/ScoringController.php
@@ -8,32 +8,21 @@ use App\Models\Score;
 use App\Models\Player;
 use App\Models\Sport;
 use App\Models\Event;
+use App\Models\User;
+
 
 class ScoringController extends Controller
 {
     public function gameSetup($id)
     {
-        $game = Game::findOrFail($id);
+        $game = Game::with('referees')->findOrFail($id);
         $teams = $game->teams;
         $sport = $game->league->sport;
-        $team1 = $teams[0];
-        $team2 = $teams[1];
-        $players1 = [];
-        foreach ($team1->players as $player) {
-            array_push($players1, $player);
-        }
-        $players2 = [];
-        foreach ($team2->players as $player) {
-            array_push($players2, $player);
-        }
-        $team1['players'] = $players1;
-        $team2['players'] = $players2;
-
+        $game['teams'][0]['players'] = Player::with('user')->where('team_id','=', $teams[0]->id)->get();
+        $game['teams'][1]['players'] = Player::with('user')->where('team_id','=', $teams[1]->id)->get();
 
         return response()->json([
             'game' => $game,
-            'team1' => $team1,
-            'team2' => $team2,
             'sport' => $sport
         ], 200);
     }
