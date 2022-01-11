35d3d4a0910c8e16371c3fe0518424a065b841fb
Arturo Rosales <arturoi_rosales@hotmail.com>
2022-01-10 19:41:26 -0600

    permisos para Gus y Julio

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\Models\User;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        //Super admin
+        $user = User::findOrFail(3);
+        $user2 = User::findOrFail(4);
+        $user->assignRole('super-admin');
+        $user2->assignRole('super-admin');
+
+
+    }
+}
